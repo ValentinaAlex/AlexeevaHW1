6d2cb8af161b649fc6cf15164f9089de924f7465
Alexeeva_Valya <sval@servinst.ru>
2022-11-21 14:10:05 +0300

    lesson 2

---
+++ BasicAlgorithms/003-MinMAX/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая в последовательности натуральных чисел определяет минимальное число оканчивающиеся на 4
+int n=4;
+int min=30000;
+for (int i=0;i<n;i++)
+{
+    int a=Convert.ToInt32(Console.ReadLine());
+    if (a<min && a%10==4) min=a;
+}
+System.Console.WriteLine(min);
---
+++ BasicAlgorithms/018/Program.cs
@@ -0,0 +1,6 @@
+﻿// 18 По двум заданным числам проверять является ли одно квадратом другого.
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+if (a*a==b) System.Console.WriteLine($"{b} является квадратом {b}");
+else
+System.Console.WriteLine("Не является");
