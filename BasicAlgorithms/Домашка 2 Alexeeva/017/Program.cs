6154bbfcaa9174c1add9690244a4ad453cf89213
Alexeeva_Valya <sval@servinst.ru>
2022-11-22 11:30:18 +0300

    AlexeevaHW2

---
+++ BasicAlgorithms/Домашка 2 Alexeeva/017/Program.cs
@@ -0,0 +1,24 @@
+﻿// 17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+System.Console.WriteLine("ВВЕДИТЕ ЧИСЛО:");
+
+int day = Convert.ToInt32(Console.ReadLine());
+string[] Days = { "ПОНЕДЕЛЬНИК", "ВТОРНИК", "СРЕДА", "ЧЕТВЕРГ", "ПЯТНИЦА", "СУББОТА", "ВОСКРЕСЕНЬЕ" };
+string txtResult = "";
+
+if (day > 5 && day < 8)
+{
+    txtResult = $". УРА! УРА! ВЫХОДНОЙ!";
+    System.Console.WriteLine($"{day}   {Days[day - 1]}{txtResult}");
+}
+else
+if (day > 0 && day < 6)
+{
+    txtResult = $". РАБОЧИЙ ДЕНЬ.";
+    System.Console.WriteLine($"{day}   {Days[day - 1]}{txtResult}");
+}
+else
+{
+    txtResult = $". Нет такого дня";
+    System.Console.WriteLine($"{day} {txtResult}");
+}
---
+++ BasicAlgorithms/Домашка 2 Alexeeva/022/Program.cs
@@ -0,0 +1,23 @@
+﻿// 22. Программа проверяет пятизначное число на палиндромом.
+Console.Clear();
+System.Console.WriteLine();
+
+
+System.Console.Write("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+ for (int i = 0; i < length / 2; i++)
+ while (number[i] == number[length - 1])
+{
+return true;
+}
+return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine($"1. Введный число - {number} - является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
