32ec6cdd0afe1b2da9fe6ac74e386ebbbd2902e2
Alexeeva_Valya <sval@servinst.ru>
2022-11-23 14:46:56 +0300

    lesson3

---
+++ Lesson 3/031/Program.cs
@@ -0,0 +1,22 @@
+﻿// 31 Сгенерировать 10 случайных чисел. Показать кубы чисел,
+// заканчивающихся на четную цифру
+/*
+int r=10;
+double t=r;// не явное приведение типов
+double x=23;
+// int o=Convert.ToInt32(x);
+int o=(int)x;//явное привидение типов
+*/
+bool Test (int a)
+{
+    //if (a%2==0) return true; else return false;
+    return a%2==0;
+}
+Random random = new Random(10);
+for (int i = 0; i < 20; i++)
+{
+    int a = random.Next(1, 100);
+    int b = a; //(int)Math.Pow(a, 3);
+    if (b % 2 == 0)
+        System.Console.WriteLine($"{a},{b}");
+}
---
+++ Lesson 3/Equlid/Program.cs
@@ -0,0 +1,16 @@
+﻿// алгоритм Евклида
+int k = 0;
+int GCD(int a, int b)
+{
+    while (a != b)
+    {
+        k++;
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+System.Console.WriteLine(GCD(1, 5000));
+System.Console.WriteLine(k);
