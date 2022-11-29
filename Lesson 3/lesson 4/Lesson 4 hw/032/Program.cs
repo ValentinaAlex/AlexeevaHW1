b4f6ba486d36afe527c07f92b1aee90af1fd8224
Alexeeva_Valya <sval@servinst.ru>
2022-11-29 19:29:03 +0300

    hw4

---
+++ Lesson 3/lesson 4/Lesson 4 hw/032/Program.cs
@@ -0,0 +1,20 @@
+﻿// 32 Задать массив из 8 целых элементов и вывести их на экран
+int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
