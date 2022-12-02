f6c1344de0f321e57d82df1dcf25e2244ab45c05
Alexeeva_Valya <sval@servinst.ru>
2022-12-02 19:44:57 +0300

    hw

---
+++ lesson 5 hw/050/Program.cs
@@ -0,0 +1,11 @@
+﻿// 50 Выяснить являются ли три числа сторонами треугольника
+
+bool CheckTriangle(int a,int b,int c)
+{
+    return a+b>c && a+c>b && b+c>a;
+}
+
+int a=8,b=6,c=12;
+
+if (CheckTriangle(a,b,c)) System.Console.WriteLine("YES"); else System.Console.WriteLine("NO");
+
