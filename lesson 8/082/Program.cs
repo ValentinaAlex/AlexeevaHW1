94b50e288ba21eeaceff3db389ecbe63998ed2dd
Alexeeva_Valya <sval@servinst.ru>
2022-12-09 15:44:25 +0300

    hw8

---
+++ lesson 8/082/Program.cs
@@ -0,0 +1,31 @@
+﻿// 082 C клавиатуры вводится строка разделенная точкой.
+// Подсчитать количество символов до точки
+
+System.Console.Write("Введите строку: ");
+ string? str = Console.ReadLine();
+ System.Console.WriteLine(str);
+
+  Tochka(str);
+
+ void Tochka (string a)
+ {
+     int count = 0;
+     for(int i=0; i < a.Length; i++)
+     {
+
+
+         if (a[i] != '.')
+             {
+
+                 //System.Console.Write(a[i]); проверял правильно считывает символы
+                 count++;
+
+
+             }
+
+         else
+             break;
+
+     }
+      System.Console.Write($"Количество символов до точки {count}");
+ }
---
+++ lesson 8/084/Program.cs
@@ -0,0 +1,17 @@
+﻿//  84 Определить являются ли введенные с клавиатуры символы правильно записью целого числа.
+// Вычислить сумму цифр введенного числа
+System.Console.WriteLine("Введите целое число: ");
+string s = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < s.Length; i++)
+
+    if (char.IsDigit(s[i]))
+        sum = sum + (int)(char.GetNumericValue(s[i]));
+    else
+    {
+        System.Console.WriteLine("Введеное число не является целым!");
+        sum = 0;
+
+        break;
+    }
+if (sum != 0) System.Console.WriteLine(sum);
